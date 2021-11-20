5f998be114f2064743584bbca4cf6be782ff917e
Magnus <32271949+magnusstrandberg@users.noreply.github.com>
2021-11-20 23:02:23 +0200

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,1 +1,13 @@
+# The inputted data is a pre_processed csv file from the original data supplied from the challenge.
+# Due to the propiotary nature of the data we have choosen not to upload it to github.
+# The pre-process is decribed in the read me.
 
+import pandas as pd
+import sys
+
+
+df = pd.read_csv("data/new_data_calculated.csv")
+
+CO2_cost_by_category = df.groupby(['CategoryL2'])['CO2kg','SpendEUR'].sum()
+
+CO2_cost_by_category.to_csv('CO2_cost_by_category.csv')
